2cebcb3d358c7b11a0c02791211956e39dcab24d
Alex Broley <alex.broley@gmail.com>
2020-10-08 21:18:17 -0700

    removed format_description from BaseTest; added  hard_fail input parameter to SQLTest

--- dataraven/tests.py
+++ dataraven/tests.py
@@ -8,12 +8,8 @@ class BaseTest(object):
         self.predicate = predicate
         self.hard_fail = hard_fail
 
-    def format_description(self, source, column, threshold):
-        return self.description.fomrat(source=source, column=column, threshold=threshold)
-
-
 
 class SQLTest(BaseTest):
-    def __init__(self, description, measure, threshold, predicate):
-        super().__init__(description, measure, threshold, predicate)
+    def __init__(self, description, measure, threshold, predicate, hard_fail=False):
+        super().__init__(description, measure, threshold, predicate, hard_fail)
 
