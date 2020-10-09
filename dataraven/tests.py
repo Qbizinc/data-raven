4f2597be04f58803f79c15a55d39e3f8bde2c188
Alex Broley <alex.broley@gmail.com>
2020-10-09 14:03:55 -0700

    added TestFactory and Test classes; removed BaseTest and SQLTest classes

--- dataraven/tests.py
+++ dataraven/tests.py
@@ -1,15 +1,91 @@
+import abc
+
+from .measures import SQLNullMeasure
+from .test_logic import test_predicate_gt
 
 
-class BaseTest(object):
-    def __init__(self, description, measure, threshold, predicate, hard_fail=False):
-        self.description = description
+class Test(object):
+    def __init__(self, descriptions, measure, threshold, predicate, hard_fail=False):
+        self.descriptions = descriptions
         self.measure = measure
         self.threshold = threshold
         self.predicate = predicate
         self.hard_fail = hard_fail
 
 
-class SQLTest(BaseTest):
-    def __init__(self, description, measure, threshold, predicate, hard_fail=False):
-        super().__init__(description, measure, threshold, predicate, hard_fail)
+class TestFactory(object):
+    def __init__(
+            self,
+            dialect,
+            from_,
+            threshold,
+            *columns,
+            where=None,
+            predicate=None,
+            hard_fail=False
+    ):
+        self.dialect = dialect
+        self.from_ = from_
+        self.threshold = threshold
+        self.columns = columns
+        self.where = where
+        self.predicate = predicate
+        self.hard_fail = hard_fail
+
+    def set_test_predicate(self, predicate):
+        self.predicate = predicate
+
+    @abc.abstractmethod
+    def build_measure(self):
+        pass
+
+    @staticmethod
+    def format_description(self, description_template, column):
+        if isinstance(self.threshold, dict):
+            threshold_ = self.threshold[column]
+        else:
+            threshold_ = self.threshold
+
+        description = description_template.format(column=column, from_clause=self.from_, threshold=threshold_)
+        return description
+
+    @abc.abstractmethod
+    def build_test_descriptions(self):
+        pass
+
+    @abc.abstractmethod
+    def factory(self):
+        pass
+
+
+class SQLNullTestFactory(TestFactory):
+    def __init__(
+            self,
+            dialect,
+            from_clause,
+            test_threshold,
+            *columns,
+            where_clause=None,
+            hard_fail=False
+    ):
+        super().__init__(dialect, from_clause, test_threshold, *columns, where=where_clause, hard_fail=hard_fail)
+
+    def build_measure(self):
+        return SQLNullMeasure(self.dialect, self.from_, *self.columns, where=self.where)
+
+    def build_test_descriptions(self):
+        test_descriptions = {}
+        description_template = "{column} in table {from_clause} should have fewer than {threshold} null values."
+        for column in self.columns:
+            description = self.format_description(description_template, column)
+            test_descriptions[column] = description
+        return test_descriptions
+
+    def factory(self):
+        descriptions = self.build_test_descriptions()
+        measure = self.build_measure()
+        predicate = test_predicate_gt
+        return Test(descriptions, measure, self.threshold, predicate, hard_fail=self.hard_fail)
+
+
 
