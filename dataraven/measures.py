ceaeb5773feed24e047937fb8b2cfe71b2dc8fbc
Alex Broley <alex.broley@gmail.com>
2020-10-11 14:05:53 -0700

    organized measure generation code

--- dataraven/measures.py
+++ dataraven/measures.py
@@ -1,26 +1,45 @@
 import abc
 
 from sqlalchemy.sql import func, distinct
+
 from .sql.helpers import compile_to_dialect
 from .sql.measure_logic import measure_proportion_each_column, measure_set_duplication
 
+from .csv.reducers import measure_null
 
-class SQLMeasure(object):
-    def __init__(self, *columns, query=None, from_=None, dialect=None):
+
+class Measure(object):
+    pass
+
+
+class SQLMeasure(Measure):
+    def __init__(self, *columns, query, from_, dialect):
+        self.columns = columns
         self.query = query
         self.from_ = from_
-        self.columns = columns
         self.dialect = dialect
 
 
-class SQLMeasureFactory(object):
+class CSVMeasure(Measure):
+    def __init__(self, *columns, path, reducer, filter, delimiter):
+        self.columns = columns
+        self.path = path
+        self.reducer = reducer
+        self.filter = filter
+        self.delimiter = delimiter
+
+
+class MeasureFactory(object):
+    @abc.abstractmethod
+    def factory(self): pass
+
+
+class SQLMeasureFactory(MeasureFactory):
     def __init__(
             self,
             dialect,
             from_,
             *columns,
-            #dialect=None,
-            #from_=None,
             where=None,
             use_ansi=True
     ):
@@ -47,7 +66,6 @@ class SQLMeasureFactory(object):
 
 class SQLNullMeasureFactory(SQLMeasureFactory):
     def __init__(self, dialect, from_, *columns, where=None, use_ansi=True):
-        #super().__init__(*columns, dialect=dialect, from_=from_, where=where, use_ansi=use_ansi)
         super().__init__(dialect, from_, *columns, where=where, use_ansi=use_ansi)
 
     def build_measure_query(self):
@@ -63,7 +81,6 @@ class SQLNullMeasureFactory(SQLMeasureFactory):
 
 class SQLDuplicateMeasureFactory(SQLMeasureFactory):
     def __init__(self, dialect, from_, *columns, where=None, use_ansi=True):
-        #super().__init__(*columns, dialect=dialect, from_=from_, where=where, use_ansi=use_ansi)
         super().__init__(dialect, from_, *columns, where=where, use_ansi=use_ansi)
 
     def build_measure_query(self):
@@ -81,10 +98,32 @@ class SQLDuplicateMeasureFactory(SQLMeasureFactory):
 
 class SQLSetDuplicateMeasureFactory(SQLMeasureFactory):
     def __init__(self, dialect, from_, *columns, where=None, use_ansi=True):
-        #super().__init__(*columns, dialect=dialect, from_=from_, where=where, use_ansi=use_ansi)
         super().__init__(dialect, from_, *columns, where=where, use_ansi=use_ansi)
 
     def build_measure_query(self):
         measure_query = measure_set_duplication(self.from_, *self.columns, where_clause=self.where)
         return measure_query
 
+
+class CSVMeasureFactory(MeasureFactory):
+    def __init__(self, path, *columns, filter=None, delimiter=','):
+        self.path = path
+        self.columns = columns
+        self.filter = filter
+        self.delimiter = delimiter
+
+    @abc.abstractmethod
+    def build_reducer(self):
+        pass
+
+    def factory(self):
+        reducer = self.build_reducer()
+        return CSVMeasure(*self.columns, self.path, reducer, self.filter, self.delimiter)
+
+
+class CSVNullMeasure(CSVMeasureFactory):
+    def __init__(self, path, *columns, filter=None, delimiter=','):
+        super().__init__(path, *columns, filter=filter, delimiter=delimiter)
+
+    def build_reducer(self):
+        return measure_null
