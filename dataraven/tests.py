0212fec9c0e9a3e76efafc98f27343bf266bc797
Alex Broley <alex.broley@gmail.com>
2020-10-12 10:45:32 -0700

    added functions to return concrete test objects

--- dataraven/tests.py
+++ dataraven/tests.py
@@ -1,6 +1,6 @@
 import abc
 
-from .measures import SQLNullMeasureFactory
+from .measures import SQLNullMeasure, CSVNullMeasure
 from .test_logic import test_predicate_gt
 
 
@@ -39,31 +39,6 @@ class CSVTest(Test):
                          hard_fail=hard_fail)
 
 
-'''def test_factory(
-        type_,
-        *columns,
-        description,
-        measure=None,
-        threshold=None,
-        predicate=None,
-        hard_fail=False,
-        custom_test=None
-):
-    expected_types = ("SQL", "CSV", "CUSTOM_SQL")
-    test_type = type_.upper()
-
-    if test_type == "SQL":
-        test = SQLTest(description, measure, threshold, predicate, hard_fail=hard_fail)
-    elif test_type == "CSV":
-        test = CSVTest(description, measure, threshold, predicate, hard_fail=hard_fail)
-    elif test_type == "CUSTOM_SQL":
-        test = CustomSQLTest(description, custom_test, *columns, threshold=threshold, hard_fail=hard_fail)
-    else:
-        exp_types = ','.join(expected_types)
-        raise ValueError(f"type_ parameter expected to be in {exp_types} but found {test_type}")
-    return test'''
-
-
 class TestFactory(object):
     def __init__(
             self,
@@ -88,60 +63,49 @@ class TestFactory(object):
 
 
 class CustomTestFactory(TestFactory):
-    def __init__( self, description, custom_test, *columns, threshold=None, hard_fail=False):
+    def __init__( self, description, custom_test, *columns, threshold=None, hard_fail=None):
         super().__init__(*columns, description=description, custom_test=custom_test, threshold=threshold,
                          hard_fail=hard_fail)
 
     def factory(self):
-        return CustomSQLTest(self.description, self.custom_test, *self.columns, self.threshold, self.hard_fail)
+        return CustomSQLTest(self.description, self.custom_test, *self.columns, threshold=self.threshold,
+                             hard_fail=self.hard_fail)
 
 
 class SQLTestFactory(TestFactory):
-    def __init__(self, measure, description, threshold, predicate, hard_fail):
+    def __init__(self, measure, description, threshold, predicate, hard_fail=False):
         super().__init__(measure=measure, description=description, threshold=threshold, predicate=predicate,
                          hard_fail=hard_fail)
 
     def factory(self):
-        return SQLTest(self.description, self.measure, self.threshold, self.predicate, self.hard_fail)
+        return SQLTest(self.description, self.measure, self.threshold, self.predicate, hard_fail=self.hard_fail)
 
 
 class CSVTestFactory(TestFactory):
-    def __init__(self, measure, description, threshold, predicate, hard_fail):
+    def __init__(self, measure, description, threshold, predicate, hard_fail=False):
         super().__init__(measure=measure, description=description, threshold=threshold, predicate=predicate,
                          hard_fail=hard_fail)
 
     def factory(self):
-        return CSVTest(self.description, self.measure, self.threshold, self.predicate, self.hard_fail)
+        return CSVTest(self.description, self.measure, self.threshold, self.predicate, hard_fail=self.hard_fail)
 
 
 def create_sql_null_test(dialect, from_, threshold, *columns, where=None, hard_fail=False, use_ansi=True):
     description = "{column} in table {from_} should have fewer than {threshold} null values."
-    measure = SQLNullMeasureFactory(dialect, from_, *columns, where=where, use_ansi=use_ansi).factory()
+    measure = SQLNullMeasure(dialect, from_, *columns, where=where, use_ansi=use_ansi).factory()
     predicate = test_predicate_gt
     test = SQLTestFactory(measure, description, threshold, predicate, hard_fail=hard_fail).factory()
     return test
 
 
-'''class SQLNullTest(TestFactory):
-    def __init__(
-            self,
-            dialect,
-            from_,
-            threshold,
-            *columns,
-            where=None,
-            hard_fail=False,
-            use_ansi=True
-    ):
-        description = "{column} in table {from_} should have fewer than {threshold} null values."
-        measure = SQLNullMeasureFactory(dialect, from_, *columns, where=where, use_ansi=use_ansi).factory()
-        predicate = test_predicate_gt
-        super().__init__(
-            description=description,
-            measure=measure,
-            threshold=threshold,
-            predicate=predicate,
-            hard_fail=hard_fail
-        )'''
+def create_csv_null_test(path, threshold, *columns, delimiter=',', hard_fail=False):
+    description = "{column} in document {from_} should have fewer than {threshold} null values."
+    measure = CSVNullMeasure(path, *columns, delimiter=delimiter).factory()
+    predicate = test_predicate_gt
+    test = CSVTestFactory(measure, description, threshold, predicate, hard_fail=hard_fail).factory()
+    return test
 
 
+def create_custom_sql_test(description, test_query, *columns, threshold=None, hard_fail=None):
+    test = CustomTestFactory(description, test_query, *columns, threshold=threshold, hard_fail=hard_fail).factory()
+    return test
