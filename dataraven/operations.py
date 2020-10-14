b6ceeaee84c45d33f3b47d3785cbf23754080443
Alex Broley <alex.broley@gmail.com>
2020-10-14 16:12:31 -0700

    refactored all operations classes to give cleaner, stream lined presentation

--- dataraven/operations.py
+++ dataraven/operations.py
@@ -7,32 +7,15 @@ from .csv.operations import get_csv_document, apply_reducer
 from .csv.measure_logic import build_measure_proportion_values
 
 
-class Operator(object):
-    @staticmethod
-    def format_test_description(test, **kwargs):
-        test_descriptions = {}
-        description_template = test.description
-        threshold = test.threshold
-        measure = test.measure
-        columns = measure.columns
-        from_ = measure.from_
-        kwargs["from_"] = from_
-        for column in columns:
-            if isinstance(threshold, dict):
-                threshold_ = threshold[column]
-            else:
-                threshold_ = threshold
-            kwargs["threshold"] = threshold_
-            kwargs["column"] = column
-            description = description_template.format(**kwargs)
-            test_descriptions[column] = description
-        return test_descriptions
+class Operations(object):
+    def __init__(self, logger, test):
+        self.logger = logger
+        self.test = test
 
-    @staticmethod
-    def build_test_outcomes(measure_values, test):
+    def build_test_outcomes(self, measure_values):
         test_outcomes = []
-        threshold = test.threshold
-        predicate = test.predicate
+        threshold = self.test.threshold
+        predicate = self.test.predicate
         for column in measure_values:
             if isinstance(threshold, dict):
                 threshold_ = threshold[column]
@@ -50,33 +33,29 @@ class Operator(object):
         test_result_msgs = []
         for test_outcome in test_outcomes:
             column = test_outcome["column"]
-            description = test_descriptions.get(column)
-            if description is None:
-                description = test_descriptions.get("no_column")
+            description = test_descriptions[column]
             result = test_outcome["result"]
             measure = test_outcome["measure"]
             threshold = test_outcome["threshold"]
 
             test_result_msg = f"""
-                                  Test description: {description}
-                                  Test outcome: {result}
-                                  Test measure: {measure}
-                                  Test threshold: {threshold}
-                                  """
+                                      Test description: {description}
+                                      Test outcome: {result}
+                                      Test measure: {measure}
+                                      Test threshold: {threshold}
+                                      """
             test_result_msg = {"result_msg": test_result_msg, "outcome": result, "column": column}
             test_result_msgs.append(test_result_msg)
         return test_result_msgs
 
-    @staticmethod
-    def log_test_results(test_results, logger):
+    def log_test_results(self, test_results):
         for test_result in test_results:
             result_msg = test_result["result_msg"]
-            logger(result_msg)
+            self.logger(result_msg)
         return True
 
-    @staticmethod
-    def raise_execpetion_if_fail(test_result_msgs, test):
-        hard_fail = test.hard_fail
+    def raise_execpetion_if_fail(self, test_result_msgs):
+        hard_fail = self.test.hard_fail
         for test_result in test_result_msgs:
             if isinstance(hard_fail, dict):
                 column = test_result["column"]
@@ -88,24 +67,60 @@ class Operator(object):
                 if outcome == "test_fail":
                     result_msg = test_result["result_msg"]
                     error_msg = f""" 
-                    Hard fail condition met.
-                    {result_msg}
-                    """
+                        Hard fail condition met.
+                        {result_msg}
+                        """
                     raise TestFailure(error_msg)
         return True
 
     @abc.abstractmethod
-    def calculate_measure_values(self): pass
+    def format_test_description(self, *args, **kwargs): pass
 
     @abc.abstractmethod
-    def execute(self): pass
+    def calculate_measure_values(self): pass
+
+    def execute(self):
+        descriptions = self.format_test_description()
+
+        measure_values = self.calculate_measure_values()
+        test_outcomes = self.build_test_outcomes(measure_values)
+        result_msgs = self.format_test_result_msgs(test_outcomes, descriptions)
+
+        self.log_test_results(result_msgs)
+        self.raise_execpetion_if_fail(result_msgs)
+
+        test_results = {
+            "measure_values": measure_values,
+            "threshold": self.test.threshold,
+            "test_outcomes": test_outcomes,
+            "result_messages": result_msgs
+        }
+        return test_results
 
 
-class SQLOperator(Operator):
+class SQLOperations(Operations):
     def __init__(self, conn, logger, test):
-        self.test = test
-        self.logger = logger
+        super().__init__(logger, test)
         self.conn = conn
+
+    def format_test_description(self, **description_kwargs):
+        descriptions = {}
+        description_template = self.test.description
+        threshold = self.test.threshold
+        measure = self.test.measure
+        columns = measure.columns
+        from_ = measure.from_
+        description_kwargs["from_"] = from_
+        for column in columns:
+            if isinstance(threshold, dict):
+                threshold_ = threshold[column]
+            else:
+                threshold_ = threshold
+            description_kwargs["threshold"] = threshold_
+            description_kwargs["column"] = column
+            description = description_template.format(**description_kwargs)
+            descriptions[column] = description
+        return descriptions
 
     def calculate_measure_values(self):
         measure = self.test.measure
@@ -113,31 +128,52 @@ class SQLOperator(Operator):
         measure_values = FetchQueryResults(self.conn, query).get_results()
         return measure_values
 
-    def execute(self):
-        descriptions = self.format_test_description(self.test)
 
-        measure_values = self.calculate_measure_values()
-        test_outcomes = self.build_test_outcomes(measure_values, self.test)
-        result_msgs = self.format_test_result_msgs(test_outcomes, descriptions)
+class SQLSetOperations(SQLOperations):
+    def __init__(self, conn, logger, test):
+        super().__init__(conn, logger, test)
 
-        self.log_test_results(result_msgs, self.logger)
-        self.raise_execpetion_if_fail(result_msgs, self.test)
+    def format_test_description(self, **description_kwargs):
+        descriptions = {}
+        description_template = self.test.description
+        threshold = self.test.threshold
+        description_kwargs["threshold"] = threshold
+        measure = self.test.measure
+        columns = measure.columns
+        columns_ = ", ".join(columns)
+        description_kwargs["column"] = columns_
+        from_ = measure.from_
+        description_kwargs["from_"] = from_
+        description = description_template.format(**description_kwargs)
+        columns_ = str(columns)
+        descriptions[columns_] = description
+        return descriptions
 
-        test_results = {
-            "measure_values": measure_values,
-            "threshold": self.test.threshold,
-            "test_outcomes": test_outcomes,
-            "result_messages": result_msgs
-        }
-        return test_results
 
-
-class CSVOperator(Operator):
+class CSVOperations(Operations):
     def __init__(self, logger, test, fieldnames=None, **reducer_kwargs):
-        self.test = test
-        self.logger = logger
+        super().__init__(logger, test)
         self.fieldnames = fieldnames
         self.reducer_kwargs = reducer_kwargs
+
+    def format_test_description(self, **description_kwargs):
+        descriptions = {}
+        description_template = self.test.description
+        threshold = self.test.threshold
+        measure = self.test.measure
+        columns = measure.columns
+        from_ = measure.from_
+        description_kwargs["from_"] = from_
+        for column in columns:
+            if isinstance(threshold, dict):
+                threshold_ = threshold[column]
+            else:
+                threshold_ = threshold
+            description_kwargs["threshold"] = threshold_
+            description_kwargs["column"] = column
+            description = description_template.format(**description_kwargs)
+            descriptions[column] = description
+        return descriptions
 
     def calculate_measure_values(self):
         measure = self.test.measure
@@ -145,47 +181,43 @@ class CSVOperator(Operator):
         path = measure.from_
         reducer = measure.reducer
         columns = measure.columns
-
         document = get_csv_document(path, delimiter=delimiter, fieldnames=self.fieldnames)
-
         reducer_results = apply_reducer(document, reducer, *columns, **self.reducer_kwargs)
         measure_values = build_measure_proportion_values(reducer_results)
         return measure_values
 
-    def execute(self):
-        descriptions = self.format_test_description(self.test)
 
-        measure_values = self.calculate_measure_values()
-        test_outcomes = self.build_test_outcomes(measure_values, self.test)
-        result_msgs = self.format_test_result_msgs(test_outcomes, descriptions)
+class CSVSetOperations(CSVOperations):
+    def __init__(self, logger, test, fieldnames=None, **reducer_kwargs):
+        super().__init__(logger, test, fieldnames=fieldnames, **reducer_kwargs)
 
-        self.log_test_results(result_msgs, self.logger)
-        self.raise_execpetion_if_fail(result_msgs, self.test)
+    def format_test_description(self, **description_kwargs):
+        descriptions = {}
+        description_template = self.test.description
+        threshold = self.test.threshold
+        description_kwargs["threshold"] = threshold
+        measure = self.test.measure
+        columns = measure.columns
+        columns_ = ", ".join(columns)
+        description_kwargs["column"] = columns_
+        from_ = measure.from_
+        description_kwargs["from_"] = from_
+        description = description_template.format(**description_kwargs)
+        descriptions[columns] = description
+        return descriptions
 
-        test_results = {
-            "measure_values": measure_values,
-            "threshold": self.test.threshold,
-            "test_outcomes": test_outcomes,
-            "result_messages": result_msgs
-        }
-        return test_results
 
-
-class CustomOperator(Operator):
+class CustomSQLOperations(Operations):
     def __init__(self, conn, logger, test, **test_desc_kwargs):
-        self.test = test
-        self.logger = logger
+        super().__init__(logger, test)
         self.conn = conn
         self.test_desc_kwargs = test_desc_kwargs
 
-    def calculate_measure_values(self): pass
-
-    @staticmethod
-    def format_test_description(test, **kwargs):
+    def format_test_description(self, **kwargs):
         test_descriptions = {}
-        description_template = test.description
-        columns = test.columns
-        threshold = test.threshold
+        description_template = self.test.description
+        columns = self.test.columns
+        threshold = self.test.threshold
 
         if columns:
             for column in columns:
@@ -223,11 +255,11 @@ class CustomOperator(Operator):
         return test_outcomes
 
     def execute(self):
-        descriptions = self.format_test_description(self.test, **self.test_desc_kwargs)
+        descriptions = self.format_test_description(**self.test_desc_kwargs)
         test_outcomes = self.calcualte_test_results()
         result_msgs = self.format_test_result_msgs(test_outcomes, descriptions)
-        self.log_test_results(result_msgs, self.logger)
-        self.raise_execpetion_if_fail(result_msgs, self.test)
+        self.log_test_results(result_msgs)
+        self.raise_execpetion_if_fail(result_msgs)
 
         test_results = {
             "test_outcomes": test_outcomes,
