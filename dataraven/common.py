1778519213651307b2a5e0da2db5b9e682ef8b5b
Alex Broley <alex.broley@gmail.com>
2020-10-15 10:04:03 -0700

    added static/test_result_message_template.txt; added helper functions for loading templates from static

---
+++ dataraven/common.py
@@ -0,0 +1,12 @@
+
+
+def load_template(path):
+    with open(path, 'r') as infile:
+        template = infile.read()
+    infile.close()
+    return template
+
+
+def test_reuslt_msg_template():
+    path = "dataraven/static/test_result_message_template.txt"
+    return load_template(path)
