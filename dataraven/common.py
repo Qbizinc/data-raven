7a5325b076b3afc615c862c80af19e3f2e2b935f
Alex Broley <alex.broley@gmail.com>
2020-10-15 10:31:01 -0700

    added hard_fail_msg_template

--- dataraven/common.py
+++ dataraven/common.py
@@ -10,3 +10,8 @@ def load_template(path):
 def test_reuslt_msg_template():
     path = "dataraven/static/test_result_message_template.txt"
     return load_template(path)
+
+
+def hard_fail_msg_template():
+    path = "dataraven/static/hard_fail_message_template.txt"
+    return load_template(path)
